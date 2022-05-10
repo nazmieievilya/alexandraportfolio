535ddd9a0c667117f9eb4ca9732290cda33f43b0
nazmieievilya <ilyanazmeev111@gmail.com>
2022-05-10 10:37:55 +0300

    works
    added works

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,24 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-fontawesome-css`,
-
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
 
 
   ],
--- src/pages/components/App.js
+++ src/pages/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './Header'
 import  Home  from './Home'
+import Works from './Works'
+
+
 const App = () => {
   return (
     <>
       <Header />
       <Home />
+      <Works />
     </>
   )
 }
